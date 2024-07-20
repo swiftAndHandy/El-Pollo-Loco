eb40f84eff1c43d041d24df593ce8efd40182ce9
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-07-20 19:59:49 +0200

    added some JSdoc

--- assets/js/classes/world.class.js
+++ assets/js/classes/world.class.js
@@ -4,12 +4,11 @@ class World {
     keyboard = new Keyboard();
     gamepad = new Gamepad();
 
-    // speedLimiter = 30;
-
     camera = {
         x: 0,
         y: 0,
         offset: 150,
+        cutscenePlays: false,
     };
 
     framerate = {
@@ -39,7 +38,6 @@ class World {
         this.ctx = canvas.getContext('2d');
         this.framerate.msPerFrame = 1000 / this.framerate.fps;
         this.time.msPrev = window.performance.now();
-        // this.speedLimiter = this.framerate.fps / this.speedLimiter;
         // setInterval(() => {
         //     console.log(this.framerate.frame);
         // }, 1000);
@@ -52,41 +50,69 @@ class World {
      * @returns {boolean}
      */
     pause() {
-        if (!this.time.preventPause) {
+        if (!this.time.preventPause && !this.camera.cutscenePlays) {
             this.time.paused = !this.time.paused;
-            this.time.paused ? this.pauseAudio() : this.replayAudio();
+            this.time.paused ? this.pauseAudio() : this.continueAudio();
+        } else if (this.camera.cutscenePlays && !this.time.paused) {
+            this.time.paused = true;
+            this.pauseAudio();
         }
 
         return this.time.paused;
     }
 
+
+    /**
+     * pauses Audio on pause and some audio on cutscenes
+     */
     pauseAudio() {
-        this.audio.currentlyPlayed.forEach(audioElement => {
-            audioElement.pause();
-        });
+        if (!this.camera.cutscenePlays) {
+            this.audio.currentlyPlayed.forEach(audioElement => {
+                audioElement.pause();
+            });
+        } else {
+
+        }
     }
 
-    replayAudio() {
+
+    /**
+     * continues the playback of audio-files. 
+     */
+    continueAudio() {
         this.audio.currentlyPlayed.forEach(audioElement => {
             audioElement.play();
         });
     }
 
+
+    /**
+     * during the game is pause, allow to end the pause by checking for inputs of those two methods
+     */
     checkForEndOfPause() {
         const gamepadUsed = this.gamepad.checkInput();
         !gamepadUsed && this.keyboard.handlePauseMenu();
     }
 
+
+    /**
+     * get the current ms the window exists, to calculate frames based on this information.
+     */
     setUpTime() {
         this.time.msNow = window.performance.now();
         this.time.msPassed = this.time.msNow - this.time.msPrev;
     }
 
+
+    /**
+     * sets the time for the last frame and updates the current frames number
+     */
     updateTime() {
         this.time.bufferTime = this.time.msPassed % this.framerate.msPerFrame;
         this.time.msPrev = this.time.msNow - this.time.bufferTime;
         this.framerate.frame++;
     }
+
 
     requestFrame() {
         window.requestAnimationFrame(() => {
@@ -94,6 +120,10 @@ class World {
         });
     }
 
+    /**
+     * updates the camera, based on the characters position.
+     * @param {number} state - 1 updates the camera, 0 sets it back to normal.
+     */
     updateCamera(state) {
         this.camera.x = -this.character.position.x;
         if (state === 1) {
@@ -104,26 +134,41 @@ class World {
     }
 
 
+    /**
+     * Draws the canvas and starts methods to handle inputs, if the game is not paused and no cutscene is shown.
+     * @returns {undefined} - used to interrupt the draw method, if no frame-update is required
+     */
     draw() {
         this.requestFrame();
         this.setUpTime();
         if (this.time.msPassed < this.framerate.msPerFrame) return;
-        if (!this.time.paused) {
+        if (!this.time.paused && !this.camera.cutscenePlays) {
             this.updateTime();
             this.ctx.clearRect(0, 0, canvas.width, canvas.height);
             this.drawWorld();
             this.getInputs();
             this.animateWorld();
+        } else if (this.camera.cutscenePlays) {
+
         } else {
             this.checkForEndOfPause();
         }
     }
 
+
+    /**
+     * Gets the input of the InputDevice
+     */
     getInputs() {
         const gamepadUsed = this.gamepad.checkInput();
         !gamepadUsed && this.keyboard.handleKeyboardInput();
     }
 
+
+    /**
+     * Draws various objects to the canvas after updating the camera position. 
+     * Set the camera back after drawing.
+     */
     drawWorld() {
         this.updateCamera(1);
         this.addObjectsToMap(this.level.backgroundObjects);
@@ -133,6 +178,11 @@ class World {
         this.updateCamera(0);
     }
 
+    /**
+     * animates some objects of the world.
+     * background objects are animated if  the character is moving.
+     * otherwise there is no need to do this, so the performance is saved.
+     */
     animateWorld() {
         this.animateObjects(this.level.clouds);
         this.animate(this.character);
@@ -144,12 +194,20 @@ class World {
     }
 
 
+    /**
+     * Helpmethod to draw arrays on the canvas
+     * @param {Array} object - the object-Array thats need to be drawn.
+     */
     addObjectsToMap(object) {
         object.forEach(o => {
             this.addToMap(o);
         })
     }
 
+    /**
+     * Draws mo on canvas. If the object is mirrored, transform it with scale(-1, 1) and translate to object to keep it on the correct place.
+     * @param {Object} mo - a single Object, e. g. BackgroundObject/Character/Enemies, that needs to be drawn on canvas.
+     */
     addToMap(mo) {
         if (mo.appearance.mirrored) {
             this.ctx.save();
@@ -164,12 +222,21 @@ class World {
         }
     }
 
+
+    /**
+     * Help method for animate
+     * @param {Array} object - contains various Objects that needs to be animated.
+     */
     animateObjects(object) {
         object.forEach(o => {
             o.animate();
         });
     }
 
+    /**
+     * Help method to trigger the animate function of an movable Object.
+     * @param {Object} target - a ClassObject, that needs to be animated.
+     */
     animate(target) {
         target.animate();
     }
